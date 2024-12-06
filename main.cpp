cf3b7735b06c6dd4e20aca1632d855d9a637c1e2
Dmytro Prokhach <dprokhach@gmail.com>
2024-12-06 17:57:56 +0200

    Added Median calculation to math method

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <chrono>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -17,62 +18,64 @@ public:
     void SetName(string N);
     string GetName();
     void printPerson();
-
-    void calculateGrade();
+    void calculateGrade(char choice);
 };
 
-Person::Person() {
-    name = "test";
-    surname = "test";
-    HW = {};
-    exam = 0;
-    grade = 0;
+Person::Person() : name("test"), surname("test"), exam(0), grade(0) {}
+
+Person::Person(string A, string B, vector<double> C, double D, double E)
+        : name(A), surname(B), HW(C), exam(D), grade(E) {}
+
+Person::~Person() {}
+
+void Person::SetName(string N) {
+    name = N;
 }
 
-Person::Person(string A, string B, vector<double> C, double D, double E) {
-    name = A;
-    surname = B;
-    HW = C;
-    exam = D;
-    grade = E;
+string Person::GetName() {
+    return name;
 }
 
-Person::~Person() {
-    name.clear();
-    surname.clear();
-    HW.clear();
-    exam = 0;
-    grade = 0;
-}
-
-void Person::SetName(string N) { name = N; }
-void Person::calculateGrade(){
-    double sum = exam;
-    for (double hw : HW){
-        sum += hw;
-    }
-    grade = sum / (HW.size() + 1);
-}
-string Person::GetName() { return name; }
 void Person::printPerson() {
     cout << name << " " << surname << " " << grade << endl;
 }
 
-int main(){
-    Person A;
-    A.printPerson();
+void Person::calculateGrade(char choice) {
+    double final_grade;
+    HW.push_back(exam);
+
+    if (choice == 'A') {
+        double sum = 0;
+        for (double hw : HW) {
+            sum += hw;
+        }
+        final_grade = sum / HW.size();
+    } else if (choice == 'M') {
+        sort(HW.begin(), HW.end());
+        size_t size = HW.size();
+        if (size % 2 == 0) {
+            final_grade = (HW[size / 2 - 1] + HW[size / 2]) / 2.0;
+        } else {
+            final_grade = HW[size / 2];
+        }
+    }
+    grade = final_grade;
+}
+
+int main() {
     vector<Person> arr;
     for (int i = 0; i < 1; i++) {
         cout << "Please input student name and surname: ";
         string TN, TS;
         cin >> TN >> TS;
+
         cout << "Please input student HW points:" << endl;
         vector<double> THW;
-
-        for (int j = 0; j < 3; j++) {
-            cout << "Please input points of HW" << j + 1 << ": ";
-            double n;
+        double n;
+        while (true) {
+            cout << "Enter points for homework: ";
             cin >> n;
+            if (n < 0) break;
             THW.push_back(n);
         }
 
@@ -80,14 +83,20 @@ int main(){
         double TE;
         cin >> TE;
 
+        char choice;
+        cout << "Choose grade calculation method (A for Average, M for Median): ";
+        cin >> choice;
+
         Person student(TN, TS, THW, TE, 0);
-        student.calculateGrade();
+        student.calculateGrade(choice);
         arr.push_back(student);
     }
 
-    cout << "Name       Surname       Final_Point(Aver.)" << endl;
-    cout << "-------------------------------------------" << endl;
+    cout << "Name       Surname       Final_Point" << endl;
+    cout << "------------------------------------" << endl;
     for (auto &var : arr) {
         var.printPerson();
     }
+
+    return 0;
 }
