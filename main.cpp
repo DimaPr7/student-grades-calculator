2db5a912f84f27837a2756346a1aeb2eeb4e02c6
Dmytro Prokhach <dprokhach@gmail.com>
2024-12-08 00:02:48 +0200

    Remove possibility input grades by hands, add reading from file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <iomanip>
 #include <vector>
+#include <fstream>
+#include <string>
 #include <cstdlib>
 #include <ctime>
-#include <iomanip>
+#include <sstream>
+#include <algorithm>
 
 using namespace std;
 
@@ -14,97 +18,114 @@ private:
     double grade;
 public:
     Person();
-    Person(string A, string B, vector<double> C, double D, double E);
+    Person(string A, string B, vector<double> C, double D);
     ~Person();
     void SetName(string N);
-    string GetName();
-    string GetSurname();
-    double GetGrade();
-    void printPerson();
-    void calculateGrade();
+    string GetName() const;
+    void printPerson() const;
+    void calculateGrade(char method);
 };
 
-Person::Person() : name("test"), surname("test"), exam(0), grade(0) {}
+Person::Person() {
+    name = "test";
+    surname = "test";
+    HW = {};
+    exam = 0;
+    grade = 0;
+}
 
-Person::Person(string A, string B, vector<double> C, double D, double E)
-        : name(A), surname(B), HW(C), exam(D), grade(E) {}
+Person::Person(string A, string B, vector<double> C, double D) {
+    name = A;
+    surname = B;
+    HW = C;
+    exam = D;
+    grade = 0;
+}
 
 Person::~Person() {
     name.clear();
     surname.clear();
     HW.clear();
+    exam = 0;
+    grade = 0;
 }
 
 void Person::SetName(string N) { name = N; }
-string Person::GetName() { return name; }
-string Person::GetSurname() { return surname; }
-double Person::GetGrade() { return grade; }
 
-void Person::calculateGrade() {
-    double sum = exam;
-    for (double hw : HW) {
-        sum += hw;
+void Person::calculateGrade(char method) {
+    if (method == 'A') {
+        double sum = exam;
+        for (double hw : HW) {
+            sum += hw;
+        }
+        grade = sum / (HW.size() + 1);
+    } else if (method == 'M') {
+        HW.push_back(exam);
+        sort(HW.begin(), HW.end());
+        size_t size = HW.size();
+        if (size % 2 == 0) {
+            grade = (HW[size / 2 - 1] + HW[size / 2]) / 2.0;
+        } else {
+            grade = HW[size / 2];
+        }
     }
-    grade = sum / (HW.size() + 1);
 }
 
-void Person::printPerson() {
-    cout << left << setw(10) << name
-         << setw(10) << surname
-         << setw(15) << fixed << setprecision(2) << grade << endl;
+string Person::GetName() const { return name; }
+
+void Person::printPerson() const {
+    cout << left << setw(12) << name << setw(12) << surname
+         << fixed << setprecision(2) << grade << endl;
 }
 
-void generateRandomScores(vector<double>& HW, double& exam) {
-    HW.clear();
-    for (int i = 0; i < 5; ++i) {
-        HW.push_back(rand() % 11);
+void readDataFromFile(const string& filename, vector<Person>& students) {
+    ifstream file(filename);
+    if (!file) {
+        cerr << "Error: Could not open the file!" << endl;
+        return;
     }
-    exam = rand() % 11;
+
+    string line;
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string name, surname;
+        vector<double> HW;
+        double exam;
+
+        ss >> name >> surname;
+        double temp;
+        for (int i = 0; i < 5; ++i) {
+            ss >> temp;
+            HW.push_back(temp);
+        }
+        ss >> exam;
+
+        Person student(name, surname, HW, exam);
+        students.push_back(student);
+    }
+    file.close();
 }
 
 int main() {
-    srand(static_cast<unsigned>(time(0)));
-    vector<Person> arr;
+    vector<Person> students;
 
-    int choice;
-    cout << "Choose input method:\n1. Manual input\n2. Randomly generate scores\nChoice: ";
-    cin >> choice;
+    readDataFromFile("students", students);
 
-    if (choice == 2) {
-        vector<double> THW;
-        double TE;
-        generateRandomScores(THW, TE);
-        arr.emplace_back("Random", "Student", THW, TE, 0);
-        arr.back().calculateGrade();
-    } else {
-        cout << "Please input student name and surname: ";
-        string TN, TS;
-        cin >> TN >> TS;
+    char method;
+    cout << "Choose grade calculation method (A for Average, M for Median): ";
+    cin >> method;
 
-        cout << "Please input student HW points:\n";
-        vector<double> THW;
-        for (int i = 0; i < 5; ++i) {
-            double n;
-            cout << "HW" << i + 1 << ": ";
-            cin >> n;
-            THW.push_back(n);
-        }
-
-        cout << "Please input student Final Exam points: ";
-        double TE;
-        cin >> TE;
-
-        arr.emplace_back(TN, TS, THW, TE, 0);
-        arr.back().calculateGrade();
+    for (auto& student : students) {
+        student.calculateGrade(method);
     }
 
-    cout << left << setw(10) << "Name"
-         << setw(10) << "Surname"
-         << setw(15) << "Final_Point(Aver.)" << endl;
+    cout << left << setw(12) << "Name" << setw(12) << "Surname"
+         << "Final Grade" << endl;
     cout << "-------------------------------------------" << endl;
 
-    for (const auto& var : arr) {
-        var.printPerson();
+    for (const auto& student : students) {
+        student.printPerson();
     }
+
     return 0;
 }
